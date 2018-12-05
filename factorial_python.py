d85876e1d638e88d54a307e573cd813acfd5f2f3
parkgwanjong <43779165+parkgwanjong@users.noreply.github.com>
2018-12-05 11:27:08 +0900

    Update factorial_python.py
    function comment added (korean ver.)

--- factorial_python.py
+++ factorial_python.py
@@ -1,14 +1,14 @@
-# Python program to find the factorial of a number provided by the user.
+# 사용자가 제공 한 숫자의 계승을 찾는 파이썬 프로그램.
 
-# change the value for a different result
+# 다른 결과에 대한 값 변경
 num = 10
 
-# uncomment to take input from the user
-#num = int(input("Enter a number: "))
+# 사용자로부터 입력을 받도록 하려면 아래의 주석 처리를 해제하십시오.
+# num = int(input("Enter a number: "))
 
 factorial = 1
 
-# check if the number is negative, positive or zero
+# 숫자가 음수, 양수 또는 0인지 확인하십시오.
 if num < 0:
    print("Sorry, factorial does not exist for negative numbers")
 elif num == 0:
