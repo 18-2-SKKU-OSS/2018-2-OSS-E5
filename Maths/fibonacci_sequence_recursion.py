64731d76a7d586e6abaed9a02e3d5e8c5a4a0f90
Pleiadex <rudobco0825@gmail.com>
2018-12-05 18:13:05 +0900

    edit comments in fibonacci_sequence_recursion.py
    Signed-off-by: Pleiadex <rudobco0825@gmail.com>

--- Maths/fibonacci_sequence_recursion.py
+++ Maths/fibonacci_sequence_recursion.py
@@ -2,11 +2,11 @@
 
 def recur_fibo(n):
     if n <= 1:
-        return n
+        return n #finish condition
     else:
-        (recur_fibo(n-1) + recur_fibo(n-2))
+        (recur_fibo(n-1) + recur_fibo(n-2)) #call recur_fibo recursivly
 
-def isPositiveInteger(limit):
+def isPositiveInteger(limit): #restrict that input is always positive
     return limit >= 0
 
 def main():
