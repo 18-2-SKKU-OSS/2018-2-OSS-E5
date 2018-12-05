9b06a221e0c4256dea427b3b3eff12f4db545d5a
kwk2696 <38908132+kwk2696@users.noreply.github.com>
2018-12-06 01:55:11 +0900

    jump_search.py code update

--- searches/jump_search.py
+++ searches/jump_search.py
@@ -1,4 +1,15 @@
+"""
+점프 탐색 (Jump Search) 
+이진 탐색과 마찬가지로 정렬된 배열을 대상으로 하는 탐색 알고리즘이다. 타겟 값보다 큰 값이 나올 때까지 
+순차적으로 배열요소에 접근을 하게 된다. 그리고
+"""
 from __future__ import print_function
+
+try:
+    raw_input          # Python 2
+except NameError:
+    raw_input = input  # Python 3
+    
 import math
 def jump_search(arr, x):
     n = len(arr)
@@ -19,8 +30,28 @@ def jump_search(arr, x):
     return -1
 
 
+#입력값이 정렬이 됬는지 확인 해주는 함수
+def __assert_sorted(collection):
+    if collection != sorted(collection):
+        print('error: Collection must be sorted')
+        raise ValueError('Collection must be sorted')
+    return True
 
-arr = [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
-x = 55
-index = jump_search(arr, x)
-print("\nNumber " + str(x) +" is at index " + str(index));
+if __name__ == '__main__':
+    import sys
+    user_input = raw_input('Enter numbers separated by comma:\n').strip()
+    collection = [int(item) for item in user_input.split(',')]
+    try:
+        __assert_sorted(collection)
+    except ValueError:
+        sys.exit('Sequence must be sorted to apply binary search')
+
+    target_input = raw_input('Enter a single number to be found in the list:\n')
+    target = int(target_input)
+    
+    #binary_search 함수 사용
+    result = jump_search(collection, target)
+    if result is not None:
+        print('{} jump search found at positions: {}'.format(target, result))
+    else:
+        print('Not found')
