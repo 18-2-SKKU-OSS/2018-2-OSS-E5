a41c65567e60fafafe3350f5dae1c6bbc9639a5d
HeoJongWon <37110949+HeoJongWon@users.noreply.github.com>
2018-12-06 04:41:27 +0900

    Update insertion_sort.py

--- sorts/insertion_sort.py
+++ sorts/insertion_sort.py
@@ -1,24 +1,14 @@
 """
-This is a pure python implementation of the insertion sort algorithm
-
-For doctests run following command:
-python -m doctest -v insertion_sort.py
-or
-python3 -m doctest -v insertion_sort.py
-
-For manual testing run:
-python insertion_sort.py
+파이썬으로 insertion sort를 구현한 코드입니다.
+삽입 정렬 이라고 부르며 자료배열의 모든요소를 앞에서부터 차례대로 
+이미 정렬된 배열 부분과 비교하여 자신의 위치를 찾아 삽입함으로써 정렬을 완성하는 알고리즘입니다.
+배열이 길어질수록 효율이 떨어지지만, 구현이 간단하다는 장점이 있습니다.
 """
 from __future__ import print_function
 
 
 def insertion_sort(collection):
-    """Pure implementation of the insertion sort algorithm in Python
-
-    :param collection: some mutable ordered collection with heterogeneous
-    comparable items inside
-    :return: the same collection ordered by ascending
-
+    """
     Examples:
     >>> insertion_sort([0, 5, 3, 2, 2])
     [0, 2, 2, 3, 5]
@@ -30,13 +20,11 @@ def insertion_sort(collection):
     [-45, -5, -2]
     """
     for index in range(1, len(collection)):
-        while index > 0 and collection[index - 1] > collection[index]:
-            collection[index], collection[index - 1] = collection[index - 1], collection[index]
-            index -= 1
+        while index > 0 and collection[index - 1] > collection[index]: #index를 줄여가며 이미 정렬된 부분과 비교하며 위치를 찾고 
+            collection[index], collection[index - 1] = collection[index - 1], collection[index] #찾은 위치에 삽입을 함으로써 정렬한다.
+            index -= 1 
 
     return collection
-
-
 if __name__ == '__main__':
     try:
         raw_input          # Python 2
