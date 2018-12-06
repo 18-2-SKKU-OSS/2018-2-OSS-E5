799c1d0fec4b92eaa00abcb5d5533513a8dc7a1b
kwk2696 <38908132+kwk2696@users.noreply.github.com>
2018-12-07 03:51:29 +0900

    Update and rename test.py to 6. fibonacci_search.py

--- searches/6. fibonacci_search.py
+++ searches/6. fibonacci_search.py
@@ -1,4 +1,8 @@
-
+"""
+피보나치 탐색 (fibonacci search)
+이진 탐색과 비슷하여 이미 정렬된 데이터의 집합들로 부터 특정한 키를 찾는 알고리즘이다. 특별하게
+피보나치 탐색은 피보나치 수열을 이용하여 탐색 구간을 정하게 된다.
+"""
 from __future__ import print_function
 import bisect
 
@@ -9,6 +13,13 @@ except NameError:
 
 #피보나치 탐색
 def fibonacci_search(arr, x, n):
+    """
+    input값은 반드시 정렬 된 채로 주어져야 합니다.
+    그러지 않으면 원하지 않는 결과값이 나올 수 있습니다.
+    :param arr : 탐색을 진행할 배열
+    :param item : 탐색을 진행할 키(key) 값
+    :return : 키 값이 있는 위치(index), 없을 경우 None
+    """
     fibMMm2 = 0;   # (m-2)'th Fibonacci No. 
     fibMMm1 = 1;   # (m-1)'th Fibonacci No. 
     fibM = fibMMm2 + fibMMm1;  # m'th Fibonacci
