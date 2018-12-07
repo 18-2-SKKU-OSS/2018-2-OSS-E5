64044a7a89d9df24019c59a6159ff8509ac9c076
parkgwanjong <43779165+parkgwanjong@users.noreply.github.com>
2018-12-07 14:37:31 +0900

    Update client.py

--- simple_client/client.py
+++ simple_client/client.py
@@ -6,24 +6,23 @@ HOST, PORT = '127.0.0.1', 1400
 
 s = socket.socket(
 
-            socket.AF_INET,     #           ADDRESS FAMILIES
-                                #Name                   Purpose
-                                #AF_UNIX, AF_LOCAL      Local communication
-                                #AF_INET                IPv4 Internet protocols
-                                #AF_INET6               IPv6 Internet protocols
-                                #AF_APPLETALK           Appletalk
-                                #AF_BLUETOOTH           Bluetooth
+            socket.AF_INET,      # address family
+                                 #Name 목적
+                                 #AF_UNIX, AF_LOCAL 지역 통신
+                                 #AF_INET IPv4 인터넷 프로토콜
+                                 # AF_INET6 IPv6 인터넷 프로토콜
+                                 #AF_APPLETALK Appletalk
+                                 #AF_BLUETOOTH 블루투스
 
-
-            socket.SOCK_STREAM  #           SOCKET TYPES
-                                #Name           Way of Interaction
+            socket.SOCK_STREAM  #           소켓 유형
+                                #Name           통신 수단
                                 #SOCK_STREAM    TCP
                                 #SOCK_DGRAM     UDP
 )
 s.connect((HOST, PORT))
 
-s.send('Hello World'.encode('ascii'))#in UDP use sendto()
-data = s.recv(1024)#in UDP use recvfrom()
+s.send('Hello World'.encode('ascii')) #in UDP use sendto()
+data = s.recv(1024)                   #in UDP use recvfrom()
 
-s.close()#end the connection
+s.close()# 연결 종료
 print(repr(data.decode('ascii')))
