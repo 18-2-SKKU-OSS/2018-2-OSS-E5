1c811b8eaf4fbcfa8328d885d4d16ab344ddeb2d
parkgwanjong <43779165+parkgwanjong@users.noreply.github.com>
2018-12-07 14:38:44 +0900

    Update server.py

--- simple_client/server.py
+++ simple_client/server.py
@@ -4,16 +4,16 @@ import socket
 
 HOST, PORT = '127.0.0.1', 1400
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#refer to client.py
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #client.py를 참조하십시오.
 s.bind((HOST, PORT))
-s.listen(1)#listen for 1 connection
+s.listen(1)                                             # 1개의 연결에 대해 listen.
 
-conn, addr = s.accept()#start the actual data flow
+conn, addr = s.accept()                                 # 실제 데이터 흐름 시작
 
 print('connected to:', addr)
 
 while 1:
-    data = conn.recv(1024).decode('ascii')#receive 1024 bytes and decode using ascii
+    data = conn.recv(1024).decode('ascii')# 1024 바이트를 받고 ascii를 사용하여 디코딩
     if not data:
         break
     conn.send((data + ' [ addition by server ]').encode('ascii'))
