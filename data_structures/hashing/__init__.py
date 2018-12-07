287080a4313230dd1f78b21ca11bd157418da5e0
parkgwanjong <43779165+parkgwanjong@users.noreply.github.com>
2018-12-07 14:33:48 +0900

    Update __init__.py

--- data_structures/hashing/__init__.py
+++ data_structures/hashing/__init__.py
@@ -1,3 +1,9 @@
+"""
+원래 주소로부터 1, 4, 9, 16, …과 같이 2차로 되는 거리만큼 떨어진 곳을 
+차례로 탐색하여 맨 처음에 나오는 빈 곳에 집어넣는데, 해싱(hashing)에 있어서 
+같은 키값을 갖는 2개의 엔트리가 충돌한 경우에 이것을 해소하는 한 방법이다.
+2차 탐색 [quadratic probing]
+"""
 from .hash_table import HashTable
 
 class QuadraticProbing(HashTable):
