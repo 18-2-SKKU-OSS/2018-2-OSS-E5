6a55608dc6da3c0bd515266f0ee8032d9f1ce9eb
HeoJongWon <37110949+HeoJongWon@users.noreply.github.com>
2018-12-07 12:50:58 +0900

    Update merge_sort_fastest.py

--- sorts/merge_sort_fastest.py
+++ sorts/merge_sort_fastest.py
@@ -1,8 +1,10 @@
-'''
-Python implementation of merge sort algorithm.
-Takes an average of 0.6 microseconds to sort a list of length 1000 items.
-Best Case Scenario : O(n)
-Worst Case Scenario : O(n)
+"""
+이 코드는 파이썬으로 Merge Sort를 구현한 코드입니다.
+병합 정렬이라고도 부르며 다음과 같이 작동합니다.
+    1.리스트의 길이가 0또는 1이면 이미 정렬된 것으로 봅니다. 그렇지 않은 경우에는
+    2.정렬되지 않은 리스트를 절반으로 잘라 비슷한 크기의 두 부분 리스트로 나눕니다.
+    3.각 부분 리스트를 재귀적으로 합병 정렬을 이용해 정렬합니다.
+    4.두 부분 리스트를 다시 하나의 정렬된 리스트로 합병합니다.
 '''
 def merge_sort(LIST):
     start = []
