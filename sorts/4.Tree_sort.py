e02a350e31230a16553f8ca916fb1bfedf518dfa
HeoJongWon <37110949+HeoJongWon@users.noreply.github.com>
2018-12-07 15:04:37 +0900

    Add to describe function 4.Tree_sort.py
    각 함수에 대해 추가적인 설명이 필요하여 한글로 설명하였습니다.

--- sorts/4.Tree_sort.py
+++ sorts/4.Tree_sort.py
@@ -11,43 +11,44 @@ Binary Search Tree는
 
 from __future__ import print_function
 
-class node():
-    # BST data structure
-    def __init__(self, val):
+class node(): #Binary Search Tree를 구현한 class
+    def __init__(self, val): #시작할때 처음 값을 node에 넣어줍니다.
         self.val = val
         self.left = None 
         self.right = None 
     
-    def insert(self,val):
+    def insert(self,val): #insert 해주는 코드로서 
         if self.val:
-            if val < self.val:
+            if val < self.val:  #root의 값보다 작을 경우 왼쪽 서브트리로
                 if self.left is None:
                     self.left = node(val)
                 else:
                     self.left.insert(val)
-            elif val > self.val:
+            elif val > self.val: #root의 값보다 클 경우 오른쪽 서브트리로 넣어줍니다.
                 if self.right is None:
                     self.right = node(val)
                 else:
                     self.right.insert(val)
         else:
             self.val = val
-
-def inorder(root, res):
-    # Recursive travesal 
-    if root:
+"""
+Binary Search Tree를 오름차순으로 출력하기위해선
+inorder 순으로 배열에 저장하여 출력을 해야하기 위해 inorder 함수를 추가하였습니다.
+"""
+def inorder(root, res):  
+    if root: 
         inorder(root.left,res)
         res.append(root.val)
         inorder(root.right,res)
 
 def treesort(arr):
-    # Build BST
+    # Binary Search Tree를 만드는 코드입니다.
     if len(arr) == 0:
         return arr
     root = node(arr[0])
     for i in range(1,len(arr)):
         root.insert(arr[i])
-    # Traverse BST in order. 
+    # 오름차순 출력을 위해 inorder 함수를 사용하였습니다.
     res = []
     inorder(root,res)
     return res
