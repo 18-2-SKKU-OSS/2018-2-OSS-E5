bd3dfbe00ce6d119b371c32d618666a742ba144a
Pleiadex <rudobco0825@gmail.com>
2018-12-08 16:25:52 +0900

    write comment
    Signed-off-by: Pleiadex <rudobco0825@gmail.com>

--- Maths/find_lcm.py
+++ Maths/find_lcm.py
@@ -2,7 +2,8 @@
 
 def find_lcm(num_1, num_2):
     max = num_1 if num_1 > num_2 else num_2
-    while (True):
+    
+    while (True): #find least common multiple counting max by one and checking whether it is lcm or not
         if ((max % num_1 == 0) and (max % num_2 == 0)):
             break
         max += 1
