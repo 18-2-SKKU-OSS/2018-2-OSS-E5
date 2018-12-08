1da8683521398a4fd4ecc79db2e59a2703786348
Pleiadex <rudobco0825@gmail.com>
2018-12-08 16:19:55 +0900

    edited the way to get input numbers in main funcion
    Signed-off-by: Pleiadex <rudobco0825@gmail.com>

--- Maths/find_hcf.py
+++ Maths/find_hcf.py
@@ -13,8 +13,8 @@ def find_hcf(num_1, num_2):
 
 
 def main():
-    num_1 = 24
-    num_2 = 34
+    num_1 = int(input("input number 1: "))
+    num_2 = int(input("input number 2: "))
     print('HCF of %s and %s is %s:' % (num_1, num_2, find_hcf(num_1, num_2)))
 
 
