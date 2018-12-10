d448c23d9ed08d76ffefa86b8a9814a19f88fb16
Pleiadex <rudobco0825@gmail.com>
2018-12-11 02:54:22 +0900

    write comment
    Signed-off-by: Pleiadex <rudobco0825@gmail.com>

--- Maths/magic_square.py
+++ Maths/magic_square.py
@@ -1,4 +1,4 @@
-"""A magic square is an N×N grid of numbers in which the entries in each row, column and main diagonal sum to the same number (equal to N(N2+1)/2)"""
+#A magic square is an N×N grid of numbers in which the entries in each row, column and main diagonal sum to the same number (equal to N(N2+1)/2)
 
 
 import numpy as np
