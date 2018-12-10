15c2f98f8b06264baacd855570c20bd0709f42ee
Pleiadex <rudobco0825@gmail.com>
2018-12-11 02:34:12 +0900

    ducci.py
    Signed-off-by: Pleiadex <rudobco0825@gmail.com>

--- Maths/ducci.py
+++ Maths/ducci.py
@@ -1,5 +1,7 @@
 """A Ducci sequence is a sequence of n-tuples of integers, sometimes known as "the Diffy game", because it is based on sequences."""
 
+
+"""From the second n-tuple onwards, it is clear that every integer in each n-tuple in a Ducci sequence is greater than or equal to 0 and is less than or equal to the difference between the maximum and minimum members of the first n-tuple. As there are only a finite number of possible n-tuples with these constraints, the sequence of n-tuples must sooner or later repeat itself. Every Ducci sequence therefore eventually becomes periodic."""
 def ducci_sequence(*ns):
 	    while True:
 	    	yield ns
