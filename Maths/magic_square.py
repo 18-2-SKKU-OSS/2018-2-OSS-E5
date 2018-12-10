b536ac70cf12170703555170aabd192a4a7b4f7d
Pleiadex <rudobco0825@gmail.com>
2018-12-11 02:57:37 +0900

    magic_square.py
    Signed-off-by: Pleiadex <rudobco0825@gmail.com>

--- Maths/magic_square.py
+++ Maths/magic_square.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# A magic square is an N×N grid of numbers in which the entries in each row, column and main diagonal sum to the same number (equal to N(N2+1)/2)
 
 print("Hi! Welcome to the magic square algorithm")
 print("Please enter the number for which you would want a magic sqaure to be printed. Please enter an odd number")
