c54519e522a341403f019562d5a60128e0f9892c
Pleiadex <rudobco0825@gmail.com>
2018-12-11 02:26:25 +0900

    ducci.py
    Signed-off-by: Pleiadex <rudobco0825@gmail.com>

--- Maths/ducci.py
+++ Maths/ducci.py
@@ -1,3 +1,5 @@
 """A Ducci sequence is a sequence of n-tuples of integers, sometimes known as "the Diffy game", because it is based on sequences."""
 
 def ducci_sequence(*ns):
+	    while True:
+	    	yield ns
