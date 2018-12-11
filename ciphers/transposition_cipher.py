e84889c39119e6492487a26292ba9427a8ad9aa1
parkgwanjong <43779165+parkgwanjong@users.noreply.github.com>
2018-12-11 17:20:08 +0900

    Update transposition_cipher.py

--- ciphers/transposition_cipher.py
+++ ciphers/transposition_cipher.py
@@ -1,3 +1,14 @@
+"""
+전치 암호(Transposition Cipher)는 치환 암호(Substitution Cipher)와 다르게 
+특정 글자를 다른 글자로 치환하지 않는다. 그 대신 글자들의 순서를 변경하여 암호화한다. 
+예를 들어 Plaintext의 첫 글자와 열 번째 글자를 바꾸고, 두 번째 글자를 다섯 번째와 바꿔서 암호화할 수 있다. 
+이렇게 단순히 순서를 변경하는 것만으로도, Ciphertext로부터 Plaintext를 한눈에 알기는 어려워진다. 
+Transposition Cipher는 이러한 점을 이용해서 위치 변경의 규칙을 정하고, 그 규칙에 맞추어 Plaintext를 암호화한다. 
+복호화는 어렵지 않다. 암호화한 규칙을 역으로 Ciphertext로부터 Plaintext를 찾을 수 있을 것이다. 
+간단히, Transpositon Chipher는 문자들을 재순서한다고 생각하면 된다.
+
+출처: http://ieatt.tistory.com/27 [IeatT]
+"""
 from __future__ import print_function
 import math
 
