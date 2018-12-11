4501ec1325505dce07bbc1d0306ff105ec99566c
kwk2696 <38908132+kwk2696@users.noreply.github.com>
2018-12-11 10:59:15 +0900

    Update PrimeCheck.py

--- Maths/PrimeCheck.py
+++ Maths/PrimeCheck.py
@@ -1,13 +1,16 @@
+#소수인지 판별하는 함수
+
 import math
+
 def primeCheck(number):
     if number % 2 == 0 and number > 2: 
         return False
     return all(number % i for i in range(3, int(math.sqrt(number)) + 1, 2))
 
 def main():
-    print(primeCheck(37))
-    print(primeCheck(100))
-    print(primeCheck(77))
+    print(primeCheck(37)) #참
+    print(primeCheck(100)) #거짓
+    print(primeCheck(77)) #거짓
 
 if __name__ == '__main__':
 	main()
