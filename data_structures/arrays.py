6cd5a726b5cce5ad12a1f532f712764fe7a3f8c3
OhSeungJin <43779162+OhSeungJin@users.noreply.github.com>
2018-12-11 11:05:38 +0900

    Update arrays.py

--- data_structures/arrays.py
+++ data_structures/arrays.py
@@ -1,5 +1,7 @@
 arr = [10, 20, 30, 40]
 arr[1] = 30 # set element 1 (20) of array to 30 배열의 하나의 원소를 할당
 print(arr)
-arr.reverse(arr) #배열을 역방향으로 만들어준다.
+arr.reverse() #배열을 역방향으로 만들어준다.
+print(arr)
+arr.sort()#배열의 원소를 오름차순으로 정리해준다.
 print(arr)
